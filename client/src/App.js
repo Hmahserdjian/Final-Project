c4134ec2cac6ca084cee1bcaf4a261c942d24159
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-05-31 13:59:56 -0400

    sd

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Reseracher</h1>
+          <h1>Researcher</h1>
         </div>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }

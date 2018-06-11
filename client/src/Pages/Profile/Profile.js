da9ff02fdd5f43507874c56c9e6da1f14d07b592
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-06-11 14:06:48 -0400

    ssgf

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -3,9 +3,34 @@ import Jumbotron from "../../Components/Jumbotron";
 import Footer from "../../Components/Footer";
 import { Container } from "../../Components/Grid";
 import Nav from "../../Components/Nav";
-import Card from "../../Components/Card"
+import Card from "../../Components/Card";
+import List from "../../Components/List";
+import API from "../../routes/api"
 
 class Profile extends Component {
+    state = {
+        researcher: []
+    };
+
+    componentDidMount() {
+        this.researcher();
+    }
+
+    researcher = () => {
+        API.research()
+            .then(res =>
+                this.setState({
+                    researcher: res.data
+                })
+            )
+            .catch(err => console.log(err));
+    };
+
+    researcher= id => {
+        API.research(id).then(res => this.researcher());
+    }
+    
+
     render() {
         return (
             <div>
@@ -16,8 +41,21 @@ class Profile extends Component {
         </Jumbotron>
 
         <Container>
-            <Card>
-                
+            <Card title="research" icon="download">
+              {this.state.researcher.length ? (
+                <List>
+                  {this.state.researcher.map(researcher => (
+                    <researcher
+                      _id={researcher._id}
+                      cardTilte={researcher.title}
+                      cardBody= {researcher.body}
+                      date= {researcher.date}  
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">No research submitted.</h2>
+              )}
             </Card>
 
    
--- models/Research.js
+++ models/Research.js
@@ -4,15 +4,12 @@ const Schema = mongoose.Schema;
 
 var researcherSchema = new Schema({
     _id: {type:Schema.Types.ObjectId, require: true },
-    username: {type: String, required: true},
-    password: {type: String, required: true},
     cardTitle: {type: String, required: true},
     cardBody: {type: String, required: true},
-    published_date: { type: Date },
-    date: {type: Date, required: Date.now},
+    date: {type: Date, required: Date.now}
     
 }, {_id:false});
 
 var researcher = mongoose.model("researcher", researcherSchema);
 
-model.exports = Research;
+model.exports = research;
